33ec47c1923125a1adc0ab64efc4e2771b0bc517
Arkadiusz Plumbaum <arekpl28@wp.pl>
2019-05-23 16:24:17 +0200

    Updates

---
+++ precache-manifest.1acdfe759c39a4ce6866d4635840bf56.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "46063cd86157e91e61876abac7a04ba6",
+    "url": "/project/index.html"
+  },
+  {
+    "revision": "cc8b6bc5c07f3bd4cea9",
+    "url": "/project/static/css/main.7359da57.chunk.css"
+  },
+  {
+    "revision": "7d0f7ebc6de8b75b3b92",
+    "url": "/project/static/js/2.7a00debf.chunk.js"
+  },
+  {
+    "revision": "cc8b6bc5c07f3bd4cea9",
+    "url": "/project/static/js/main.2016cecc.chunk.js"
+  },
+  {
+    "revision": "369bc2d987da7beb9860",
+    "url": "/project/static/js/runtime~main.4b9e7fa5.js"
+  },
+  {
+    "revision": "693051ecbbe099eb858721e40a886033",
+    "url": "/project/static/media/datavarehuset.693051ec.png"
+  },
+  {
+    "revision": "f20918208a33a268685ba4caf1e7f17c",
+    "url": "/project/static/media/kommune.f2091820.png"
+  },
+  {
+    "revision": "3721f94a39efc722d85c977502078b7f",
+    "url": "/project/static/media/new-banner.3721f94a.png"
+  }
+]);
