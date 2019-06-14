7828f22eb624bcd6597753b066d0b7331549bc31
Arkadiusz Plumbaum <arekpl28@wp.pl>
2019-06-14 17:55:14 +0200

    Updates

---
+++ precache-manifest.0380325874f2cd1782fff49e0435305d.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca3ae7fdad35ff1a14963b5d431c3cd0",
+    "url": "/project/index.html"
+  },
+  {
+    "revision": "4bd9eeb4d0225f3bd607",
+    "url": "/project/static/css/main.cbbf2895.chunk.css"
+  },
+  {
+    "revision": "af70afcd547a9b3e42e7",
+    "url": "/project/static/js/2.563af7e2.chunk.js"
+  },
+  {
+    "revision": "4bd9eeb4d0225f3bd607",
+    "url": "/project/static/js/main.b8e531f9.chunk.js"
+  },
+  {
+    "revision": "369bc2d987da7beb9860",
+    "url": "/project/static/js/runtime~main.4b9e7fa5.js"
+  },
+  {
+    "revision": "01dc34d17bafd1c7ce60c68f165f0330",
+    "url": "/project/static/media/Alta.01dc34d1.jpg"
+  },
+  {
+    "revision": "85fa08cbba63f5e42a9b9ca009403fc6",
+    "url": "/project/static/media/AndreasHammerstedt.85fa08cb.png"
+  },
+  {
+    "revision": "95d86e429fe403357fdfb92c37da1871",
+    "url": "/project/static/media/EspenSagen.95d86e42.gif"
+  },
+  {
+    "revision": "87f5d4ac122220c610d1aeee07f0f42b",
+    "url": "/project/static/media/Eva.87f5d4ac.jpg"
+  },
+  {
+    "revision": "a330bbfcdea4fdd7737acfb377428973",
+    "url": "/project/static/media/JohnGran.a330bbfc.jpg"
+  },
+  {
+    "revision": "1cfe3bbbc93c4e19889a1a7583c73314",
+    "url": "/project/static/media/KennethOyen.1cfe3bbb.jpg"
+  },
+  {
+    "revision": "e8f07640cf8c5bd9ad6b11c706e98c32",
+    "url": "/project/static/media/MikeGlrtz.e8f07640.png"
+  },
+  {
+    "revision": "3ed4e92708841c4c07572cb4da76b1c4",
+    "url": "/project/static/media/Nittedal.3ed4e927.png"
+  },
+  {
+    "revision": "0f4335e8621bcc9c0b039b3ecf5cf44d",
+    "url": "/project/static/media/Norkartresepsjon.0f4335e8.png"
+  },
+  {
+    "revision": "1d23ac54dd8bb434bcc18e91389cc616",
+    "url": "/project/static/media/article1.1d23ac54.png"
+  },
+  {
+    "revision": "9eee6dedd83dbfa8ac69ee59e0043043",
+    "url": "/project/static/media/article2.9eee6ded.png"
+  },
+  {
+    "revision": "11926ce892242df616f9c9c2d3940ebc",
+    "url": "/project/static/media/article3.11926ce8.png"
+  },
+  {
+    "revision": "66b519a422ac6ed3f7bb1a9f0c77425a",
+    "url": "/project/static/media/article4.66b519a4.png"
+  },
+  {
+    "revision": "693051ecbbe099eb858721e40a886033",
+    "url": "/project/static/media/datavarehuset.693051ec.png"
+  },
+  {
+    "revision": "61c75cb999da81d7192dcc8f4fa5c757",
+    "url": "/project/static/media/iconkommune.61c75cb9.png"
+  },
+  {
+    "revision": "f20918208a33a268685ba4caf1e7f17c",
+    "url": "/project/static/media/kommune.f2091820.png"
+  },
+  {
+    "revision": "3490340ae2b5f7fcc7ee394ba0070965",
+    "url": "/project/static/media/kvinner.3490340a.jpg"
+  },
+  {
+    "revision": "3721f94a39efc722d85c977502078b7f",
+    "url": "/project/static/media/new-banner.3721f94a.png"
+  },
+  {
+    "revision": "24cc9e293253c1f0875ba2de3434ad74",
+    "url": "/project/static/media/utvikler.24cc9e29.jpg"
+  }
+]);
